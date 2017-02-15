fe463e0b875ec7e2e20d38e22cd62b56083fa7f0
Christine <Christine@Dell>
2017-02-15 13:52:26 +0800

    Initial commit

---
+++ src/digitaldifferentialanalyzer/TestDigitalDifferentialAnalyzer.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package digitaldifferentialanalyzer;
+
+import java.util.List;
+
+/**
+ *
+ * @author Christine
+ */
+public class TestDigitalDifferentialAnalyzer {
+    public static void main(String[] args) {
+        Pair<Integer, Integer> p0 = new Pair<>(1, 7);
+        Pair<Integer, Integer> p1 = new Pair<>(6, 10);
+        
+        DigitalDifferentialAnalyzer dda = new DigitalDifferentialAnalyzer(p0, p1);
+        List<Pair> result = dda.compute();
+        for (Pair point: result) {
+            System.out.println(point.x + " " + point.y);
+        }
+    }
+}
